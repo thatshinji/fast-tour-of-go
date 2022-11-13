83da938269eeabfcf9fcdfd82a71850f0748c8f9
thatshinji <li2.y@foxmail.com>
2022-11-13 13:46:53 +0800

    feat: defer

---
+++ src/deferred/deferred_trace_log_test.go
@@ -0,0 +1,30 @@
+package deferred
+
+import (
+	"fmt"
+	"testing"
+)
+
+func trace(s string) string {
+	fmt.Println("entering:", s)
+	return s
+}
+
+func un(s string) {
+	fmt.Println("leaving:", s)
+}
+
+func a() {
+	defer un(trace("a"))
+	fmt.Println("in a")
+}
+
+func b() {
+	defer un(trace("b"))
+	fmt.Println("in b")
+	a()
+}
+
+func TestTraceLog(t *testing.T) {
+	b()
+}
